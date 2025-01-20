159d6cd1e9747b83ab1b67d1084afcad0ac92565
DevVictor19 <antoniovictor12@live.com>
2025-01-20 12:19:12 -0300

    feat: add initial setup

---
+++ server/src/api/database.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var mongoClient *mongo.Client
+
+func getDatabase() *mongo.Database {
+	return mongoClient.Database("chatbot")
+}
+
+func getConnectionUrl() string {
+	user := os.Getenv("DB_USER")
+	pwd := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+
+	return fmt.Sprintf("mongodb://%s:%s@%s:27017", user, pwd, host)
+}
+
+func initDatabaseConnection() {
+	client, err := mongo.Connect(options.Client().ApplyURI(getConnectionUrl()))
+
+	if err != nil {
+		log.Fatalf("Failed to create a new mongo client: %v", err)
+	}
+
+	mongoClient = client
+}
---
+++ server/src/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"server/src/api"
+
+	"github.com/joho/godotenv"
+)
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	loadEnv()
+
+	api.InitHttpServer()
+}
